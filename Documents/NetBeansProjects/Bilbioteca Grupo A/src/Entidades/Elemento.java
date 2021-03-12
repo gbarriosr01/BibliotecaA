0eb3b4069450f2093209adae8f0b073dd5b96f13
diegu <diegu@DESKTOP-T9PQQJ2>
2021-03-12 16:33:33 +0100

    Se vienen cositas en el canal
    -Diego-

--- Documents/NetBeansProjects/Bilbioteca Grupo A/src/Entidades/Elemento.java
+++ Documents/NetBeansProjects/Bilbioteca Grupo A/src/Entidades/Elemento.java
@@ -6,14 +6,34 @@
 package Entidades;
 
 import Validaciones.ElementoException;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  *
  * @author DAM102
  */
-public class Elemento {
+    public class Elemento implements Serializable{
+    static final long serialVersionUID = 1L;
     /*Valido: >0 Invalido: <=0 Otros: Es unico*/
     protected long id; 
     protected ArrayList <Prestamo> prestamos = new ArrayList <Prestamo>();/*Obligatorio*/
@@ -52,6 +72,12 @@ public class Elemento {
         this.id = el.id;
         
         this.genero = el.genero;
+    }
+    
+    public Elemento(long id, long idGenero) {
+        
+        this.id = id;
+        this.idGenero = idGenero;
     }
 
     public long getId() {
@@ -95,6 +121,234 @@ public class Elemento {
     
     }
     
+    //funcion 1 entrada salida
+    
+    public void exportarObjetoElementoTexto(String path) {
+        File fichero = new File(path);
+        FileWriter escritor = null;
+        PrintWriter buffer = null;
+        try {
+            try {
+                escritor = new FileWriter(fichero, true);
+                buffer = new PrintWriter(escritor);
+                buffer.print(this.data() + "\r\n");
+            } finally {
+                if (buffer != null) {
+                    buffer.close();
+                }
+                if (escritor != null) {
+                    escritor.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+    }
+    
+    //Funcion 2 entrada y salida
+    public static void exportarColeccionDeObjetosElementoTexto(String path) {
+        ArrayList<Elemento> coleccion;
+        coleccion = Elemento.convertir(Utilidades.ELEMENTOS);
+        File fichero = new File(path);
+        FileWriter escritor = null;
+        PrintWriter buffer = null;
+        try {
+            try {
+                escritor = new FileWriter(fichero, true);
+                buffer = new PrintWriter(escritor);
+                for (Elemento e : coleccion) {
+                    buffer.print(e.data() + "\n");
+                }
+            } finally {
+                if (buffer != null) {
+                    buffer.close();
+                }
+                if (escritor != null) {
+                    escritor.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+    }
+    
+    //Funcion 3 entrada y salida
+    public void exportarElementoArchivoBinario(String path) {
+        try {
+            FileOutputStream fichero = new FileOutputStream(path, true);
+            ObjectOutputStream empleado = new ObjectOutputStream(fichero);
+            empleado.writeObject(this);
+            empleado.flush();
+            empleado.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+    }
+    
+    //Funcion 4 entrada y salida
+    public static void exportarColeccionElementosaArchivoBinario(String path) {
+        ArrayList<Elemento> coleccion;
+        coleccion = Elemento.convertir(Utilidades.ELEMENTOS);
+        try {
+            FileOutputStream fichero = new FileOutputStream(path, true);
+            ObjectOutputStream escritor = new ObjectOutputStream(fichero);
+            escritor.writeObject(coleccion);
+            escritor.flush();
+            escritor.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+    }
+    
+    //Funcion 5 entrada y salida
+    public static ArrayList<Elemento> importarEmpleadoDesdeFicheroTexto(String path) {
+        ArrayList<Elemento> ret = new ArrayList<Elemento>();
+        FileReader inputStream = null;
+        BufferedReader lector = null;
+        try {
+            try {
+                inputStream = new FileReader(path);
+                lector = new BufferedReader(inputStream);
+                Elemento e;
+                while (lector.ready()) {
+                    String cadena = lector.readLine();
+                    if (cadena.isEmpty() == false) {
+                        String[] parametros = cadena.split("\\|");
+                        e = new Elemento(Long.parseLong(parametros[0]),Long.parseLong(parametros[1]) );
+                        ret.add(e);
+                    }
+                }
+            } finally {
+                if (lector != null) {
+                    lector.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (EOFException e) {
+            System.out.println("Final de fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+        return ret;
+    }
+    
+    //Funcion 6 entrada y salida
+    public static ArrayList<Elemento> importarEmpleadoDesdeFicheroBinario(String path) {
+        ArrayList<Elemento> ret = new ArrayList<Elemento>();
+        FileInputStream lector = null;
+        ObjectInputStream lectorObjeto = null;
+        try {
+            try {
+                lector = new FileInputStream(path);
+                lectorObjeto = new ObjectInputStream(lector);
+                Elemento e;
+                while ((e = (Elemento) lectorObjeto.readObject()) != null) {
+                    ret.add(e);
+                    
+                }
+            } finally {
+                if (lectorObjeto != null) {
+                    lectorObjeto.close();
+                }
+                if (lector != null) {
+                    lector.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (EOFException e) {
+            System.out.println("Final de fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se ha encontrado la clase a la cual haces referencia");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+        return ret;
+    }
+    
+    //Funcion 7 entrada y salida
+    public static ArrayList<Elemento> buscarPorIDEnFicheroDeTexto(String path) {
+        ArrayList<Elemento> ret = new ArrayList<Elemento>();
+        FileReader inputStream = null;
+        BufferedReader lector = null;
+        try {
+            try {
+                inputStream = new FileReader(path);
+                lector = new BufferedReader(inputStream);
+                Elemento e;
+                while (lector.ready()) {
+                    String cadena = lector.readLine();
+                    if (cadena.isEmpty() == false) {
+                        String[] parametros = cadena.split("\\|");
+                        e = new Elemento(Long.parseLong(parametros[0]),Long.parseLong(parametros[1]) );
+                        ret.add(e);
+                    }
+                }
+                Scanner in = new Scanner(System.in);
+                int idBuscar = 0;
+                do {
+                    System.out.print("Introduce el id que desea buscar dentro del fichero de texto: ");
+                    idBuscar = in.nextInt();
+                    if (idBuscar <= 0) {
+                        System.out.println("Debe introducir un valor mayor que cero");
+                    }
+                } while (idBuscar <= 0);
+                for (Elemento em : ret) {
+                    if (em.getId() == idBuscar) {
+                        System.out.println(em.data());
+                        break;
+                    }
+                }
+            } finally {
+                if (lector != null) {
+                    lector.close();
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("El carácter introducido no es un entero");
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha encontrado el fichero");
+        } catch (EOFException e) {
+            System.out.println("Final de fichero");
+        } catch (IOException e) {
+            System.out.println("Se ha producido un error en la inserción de los datos");
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error inesperado intentelo de nuevo");
+        }
+        return ret;
+    }
+    
+    public static ArrayList<Elemento> convertir(Elemento[] array) {
+        ArrayList<Elemento> ret = new ArrayList<Elemento>();
+        for (Elemento e : array) {
+            ret.add((Elemento) e);
+        }
+        return ret;
+    }
+    
+    
+    
     @Override
     public String toString() {
         return "Elemento{" + "id=" + id + ", prestamos=" + prestamos + ", genero=" + genero + '}';
@@ -105,7 +359,7 @@ public class Elemento {
      * @return Primary key id | idGenero
      */
     public String data() {
-        return  "|" + id + "|" + idGenero;
+        return id + "|" + idGenero;
     }
     
 }
