faf5bd88e83f6b8716d24ae5b5fa37e61899be2a
gbarriosr01 <gbarriosr01@educantabria.es>
2021-04-20 16:33:22 +0200

    Parte Guille, se vienen cositas

--- Documents/NetBeansProjects/Bilbioteca Grupo A/src/Dao/LoteDAO.java
+++ Documents/NetBeansProjects/Bilbioteca Grupo A/src/Dao/LoteDAO.java
@@ -47,8 +47,8 @@ public class LoteDAO {
                 pstmt = conn.prepareStatement("SELECT * FROM lotes");
                 ResultSet prs = pstmt.executeQuery();
                 while (prs.next()) {
-                    int id = prs.getInt("id");                  
-                    Date fechallegada = prs.getDate("fechallegada");                   
+                    long id = prs.getLong("id");
+                    Date fechallegada = prs.getDate("fechallegada");
                     long idProveedor = prs.getLong("idProveedor");
                     Lote lo = new Lote(id, fechallegada, idProveedor);
                     todosLotes.add(lo);
@@ -67,8 +67,99 @@ public class LoteDAO {
         }
         return todosLotes;
     }
-    
-    
+    public static Lote buscarLoteById(long idLote) {
+       Lote lote = null;
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("SELECT * FROM lotes WHERE id = ?");
+                pstmt.setString(1, String.valueOf(idLote));
+                ResultSet prs = pstmt.executeQuery();
+                while (prs.next()) {
+                    long id = prs.getLong("id");
+                    Date fechallegada = prs.getDate("fechallegada");
+                    long idProveedor = prs.getLong("idProveedor");
+                    lote = new Lote(id, fechallegada, idProveedor);
+                }
+                prs.close();
+                pstmt.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lote;
+    }
+    public static Lote buscarLoteByIdProveedor(long idProv) {
+        Lote lote = null;
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("SELECT * FROM lotes WHERE idProveedor = ?");
+                pstmt.setString(1, String.valueOf(idProv));
+                ResultSet prs = pstmt.executeQuery();
+                while (prs.next()) {
+                    long id = prs.getLong("id");
+                    Date fechallegada = prs.getDate("fechallegada");
+                    long idProveedor = prs.getLong("idProveedor");
+                    lote = new Lote(id, fechallegada, idProveedor);
+                }
+                prs.close();
+                pstmt.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lote;
+    }
+     public void eliminarLote(long idLote) {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("DELETE FROM lotes WHERE id = ?");
+                pstmt.setString(1, String.valueOf(idLote));
+                pstmt.executeUpdate();
+                System.out.println("Se ha eliminado el lote de la BD.");
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("NO se ha eliminado el lote de la BD.");
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     
     
