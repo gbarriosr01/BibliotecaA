d457d36780afca8ea47f5b7f1887cd01e1004cdb
DAM122 <DAM122@C1113.iesataulfo.local>
2021-04-20 16:20:55 +0200

    Se vienen Cositas de VIcente

--- Documents/NetBeansProjects/Bilbioteca Grupo A/src/Dao/SocioDAO.java
+++ Documents/NetBeansProjects/Bilbioteca Grupo A/src/Dao/SocioDAO.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,10 +21,10 @@ import java.util.logging.Logger;
  * @author DAM122
  */
 public class SocioDAO {
-    
+
     private Socio s;
     private static Connection conn;
-    
+
     public SocioDAO() {
         conn = BibliotecaBD.establecerConexion();
     }
@@ -32,7 +33,7 @@ public class SocioDAO {
         this.s = soc;
         conn = BibliotecaBD.establecerConexion();
     }
-    
+
     public ArrayList<Socio> todasSocios() {
         ArrayList<Socio> todasSocios = new ArrayList<Socio>();
         try {
@@ -44,7 +45,7 @@ public class SocioDAO {
                 pstmt = conn.prepareStatement("SELECT * FROM Socio");
                 ResultSet prs = pstmt.executeQuery();
                 while (prs.next()) {
-                    long id = prs.getInt("id");
+                    long id = prs.getLong("id");
                     String nombre = prs.getString("nombre");
                     String NIF = prs.getString("NIF");
                     String telefono = prs.getString("telefono");
@@ -65,5 +66,108 @@ public class SocioDAO {
             Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return todasSocios;
+    }
+
+    public static Socio buscarSocioById(int idSocio) {
+        Socio socio = null;
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("SELECT * FROM Socio WHERE id = ?");
+                pstmt.setString(1, String.valueOf(idSocio));
+                ResultSet prs = pstmt.executeQuery();
+                while (prs.next()) {
+                    long id = prs.getLong("id");
+                    String nombre = prs.getString("nombre");
+                    String NIF = prs.getString("NIF");
+                    String telefono = prs.getString("telefono");
+                    String direccion = prs.getString("direccion");
+                    socio = new Socio(id, nombre, NIF, telefono, direccion);
+                }
+                prs.close();
+                pstmt.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return socio;
+    }
+
+    public void eliminarSocio(int idSocio) {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("DELETE FROM Socio WHERE id = ?");
+                pstmt.setString(1, String.valueOf(idSocio));
+                pstmt.executeUpdate();
+                System.out.println("Se ha eliminado un socio de la BD.");
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("NO se ha eliminado el socio de la BD.");
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ArrayList<Socio> filtrarSocioPorNombre(String nombre) {
+        ArrayList<Socio> socios = new ArrayList<Socio>();
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = BibliotecaBD.establecerConexion();
+            }
+            try {
+                String sql;
+                Statement stmt = null;
+                stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+                sql = "SELECT * FROM Socio WHERE nombre LIKE '" + nombre + "'";
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    long id = rs.getLong("id");
+                    String nom = rs.getString("nombre");
+                    String NIF = rs.getString("NIF");
+                    String telefono = rs.getString("telefono");
+                    String direccion = rs.getString("direccion");
+                    Socio s = new Socio(id, nombre, NIF, telefono, direccion);
+                    socios.add(s);
+                }
+                rs.close();
+                stmt.close();
+
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    BibliotecaBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SocioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return socios;
     }
 }
